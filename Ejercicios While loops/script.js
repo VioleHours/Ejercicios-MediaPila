ed746ea7dde6d6424d68296f55bff98355be1b28
Viole Hours <holasoyviole@gmail.com>
2024-11-07 20:38:36 -0300

    base de Elije tu propia aventura y while loops

---
+++ Ejercicios While loops/script.js
@@ -0,0 +1,62 @@
+// ¿Sí o Sí?
+// Antes de pasar a los ejercicios de este módulo, pensemos cómo debería ser el código de un programa que le pida información obligatoria a un usuario.
+
+// Podríamos escribir el siguiente código usando un while Loop:
+
+// let input
+// while( !(input = prompt('Escriba su nombre, por favor.')) ){
+//   alert("No recibimos la información.")
+// }
+// alert("¡Gracias! Su nombre es: " + input + ".")
+// 🛎 Recordá: Un String vacío siempre es falso. Por lo tanto, podemos convertirlo en verdadero al negarlo con el signo de exclamación (!)
+
+
+// ¿Qué Imprimen Estos Loops?
+// 🛎 Recordá: En la Comunidad de P5 de Discord podrás ver cómo otras personas tuvieron las mismas dudas y cuáles fueron las distintas formas en que las resolvieron. ¡Aprovechá los canales para revisar y consultar!
+
+// Pensá cual será el resultado al correr estos códigos, sin probarlos en la consola. No hagas trampa, ¡puede haber un Loop Infinito!
+// let num = 1
+// while(num <=10){
+//   console.log(num)
+//   num += 2
+// } 
+// let num = 1
+// while(num <= 20){
+//   if(!(num % 4)){
+//     console.log(num)
+//   }
+//   num++
+// }
+// let num = 100
+// while(num < 150 ){
+//   console.log(num+1)
+//   num--
+// }
+
+// !🏅Escribí while Loops
+// En este ejercicio deberás imprimir en la consola:
+
+// Todos los números entre -10 y 19.
+// Todos los números pares entre 10 y 40.
+// Todos los números impares entre 300 y 333.
+// Todos los números entre 5 y 50 que sean divisibles por 5 y 3 al mismo tiempo.
+
+
+// 🏅Mi Primer "Bot"
+// En este ejercicio deberás crear un bot para una cafetería que reciba hasta 20 clientes y les asigne una mesa, a cada uno de ellos.
+
+// El mensaje que deberá mostrar a cada uno es: "¡Bienvenid@ a mi café! Su mesa es la X".
+
+
+// Extra Credit
+// Optimizá el código creando una Variable que almacene el número de clientes.
+
+
+// 🏅🏅Par O Impar
+// En este ejercicio deberás crear un programa que le pregunte al usuario un número y que le devuelva un mensaje que indique si es par o impar. El programa terminará si el usuario ingresa un 0 o un número negativo.
+
+// PISTA
+// Mediante la función prompt, deberá ingresar un conjunto de valores numéricos enteros y positivos.
+// Por cada número, deberás informarle si el valor ingresado es par o impar.
+// La salida del programa se producirá cuando ingrese un 0 o un número negativo.
+
